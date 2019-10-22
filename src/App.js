ce1c430974529da89b2a71fee2c6d08e326aa48c
Andrés <ga.alaix564@gmail.com>
2019-10-22 12:05:02 -0500

    Reto 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.count = this.count.bind(this);
+
+    this.state = {
+      count: 0
+    };
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button onClick={this.count} id="inc">Incrementa</button>
       </div>
     );
+  }
+
+  count() {
+    this.setState({
+      count: this.state.count + 1
+    })
   }
 }
 
 export default App;
+
